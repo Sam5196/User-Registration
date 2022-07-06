ecf3611627ee4dfb761e0a6e221db27a07ad4dfa
Sam5196 <sammedpachore1008@gmail.com>
2022-07-06 19:31:01 +0530

    Added: New Main and Test Classes to verify multiple emails with one parameterised test method

--- eclipse-workspace/JUnit/src/main/java/org/example/UserDetails.java
+++ eclipse-workspace/JUnit/src/main/java/org/example/UserDetails.java
@@ -4,6 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserDetails {
+
     public String regex;
 
     public boolean firstName(String firstName) {
@@ -12,7 +13,6 @@ public class UserDetails {
         Matcher matcher = pattern.matcher(firstName);
         return matcher.matches();
     }
-
     public boolean lastName(String lastName) {
         regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -25,23 +25,19 @@ public class UserDetails {
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
         return matcher.matches();
-
     }
-
     public boolean mobile(String mobile) {
         regex = "^[0-9]{2}(\\s){1}[0-9]{10}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(mobile);
         return matcher.matches();
     }
-
     public boolean password(String password) {
         regex = "^[a-zA-Z0-9]{8,}$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
-
     public boolean upperCase(String upperCase) {
         regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&]{1})[A-Za-z\\d@$!%*?&]{8,}$";
         Pattern pattern = Pattern.compile(regex);
